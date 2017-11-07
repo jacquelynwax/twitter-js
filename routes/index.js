f06a836d09427cbda0fdc03e7532b1b90608b857
Jacquelyn Wax <jacquelynwax@gmail.com>
2017-11-07 14:36:29 -0500

    live test a success

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,18 @@ const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetbank');
 
+router.use(express.static('public'))
+
 router.get('/', function(req, res) {
   let tweets = tweetBank.list();
   res.render('index', {tweets: tweets});
 })
 
-router.get('/stylesheets/style.css', function(req, res){
-  let options = {
-    root: __dirname + '/../public/'
-  }
-
-  res.sendFile('/stylesheets/style.css',options);
-
-})
-
-// router.get('/news')
-
-
+// router.get('/stylesheets/style.css', function(req, res){
+//   let options = {
+//     root: __dirname + '/../public/'
+//   }
+//   res.sendFile('/stylesheets/style.css',options);
+// })
 
 module.exports = router;
