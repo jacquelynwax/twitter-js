017d13591e41a2d8efed02b23229cee94be23f3e
Jacquelyn Wax <jacquelynwax@gmail.com>
2017-11-07 10:50:43 -0500

    working on nunjucks

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const nunjucks = require('nunjucks');
 const app = express();
+
+app.engine('html', nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});)
+app.set()
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+[1, 2, 3].forEach(console.log(element);)
+
+nunjucks.configure('views', {noCache: true});
 
 app.use(function(req, res, next) {
     console.log(req.method + ' ' + req.path);
     next();
   })
-
 
 app.get('/', function(req, res, next){
     res.send('<h1>Welcome to Twitter-ish</h1>')
@@ -15,11 +33,20 @@ app.get('/news', function(req, res, next){
     res.send('<h1>Welcome to the news section of Twitter-ish</h1>')
 })
 
-
-
-
 app.listen(3000, function(){
     console.log('server listening');
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
